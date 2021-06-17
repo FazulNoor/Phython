c905ed1915cd119c433208ab3ae8069506cb13c6
FazulNoor <fazul_noor@yahoo.com>
2021-06-17 19:35:42 +0000

    class

---
+++ PhyClass.py
@@ -0,0 +1,12 @@
+class Order:
+    __init__(self,orderId,orderName):
+       self.orderId = orderId
+       self.orderName = orderName
+def get_order():
+    return self.orderId
+
+objects = Order(12,'Rice')    
+print(f"Order object{object}")
+
+
+
